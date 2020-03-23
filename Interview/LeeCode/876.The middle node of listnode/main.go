235d0679c859ad403577e7ef3266b42b2a249c17
Lzyandwjr <liuzheyuan0911@gmail.com>
2020-03-23 10:31:17 +0800

    update

---
+++ Interview/LeeCode/876.The middle node of listnode/main.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+
+如果有两个中间结点，则返回第二个中间结点。
+
+
+
+示例 1：
+
+输入：[1,2,3,4,5]
+输出：此列表中的结点 3 (序列化形式：[3,4,5])
+返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+示例 2：
+
+输入：[1,2,3,4,5,6]
+输出：此列表中的结点 4 (序列化形式：[4,5,6])
+由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+*/
+//传统方法为首先遍历一次链表得到链表长度，再遍历一次，得到中间值
+//快慢链表 利用快慢指针，快指针每次步长为2，慢指针每次步长是1
+
+
+//Definition for singly-linked list.
+type ListNode struct {
+      Val int
+      Next *ListNode
+  }
+
+func middleNode(head *ListNode) *ListNode {
+	fast,slow := head,head
+	for fast != nil && fast.Next != nil{
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
+
